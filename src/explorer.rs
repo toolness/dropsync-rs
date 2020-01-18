a30f90b4411497f6f767de7cfb91eeec89a5dbc1
Atul Varma <varmaa@gmail.com>
2020-01-18 06:41:37 -0500

    Fail more gracefully in open_in_explorer().

--- src/explorer.rs
+++ src/explorer.rs
@@ -2,13 +2,14 @@ use std::path::PathBuf;
 use std::process::Command;
 
 pub fn open_in_explorer(path: &PathBuf) {
+    println!("Opening {}.", path.to_string_lossy());
     if cfg!(target_os = "windows") {
-        println!("Opening {}.", path.to_string_lossy());
         Command::new("explorer")
           .arg(&path.as_os_str())
           .spawn()
           .unwrap();
     } else {
-        unimplemented!();
+        println!("Oops, I don't know how to open it on this OS.");
+        println!("Please open it yourself. Sorry!");
     }
 }
