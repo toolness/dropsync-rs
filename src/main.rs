8a455eb98975f9b1606175685ca2f24ab21b1462
Atul Varma <varmaa@gmail.com>
2021-07-24 13:11:14 -0400

    Bump version to 1.3.0.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod explorer;
 
 use dir_state::DirState;
 
-const VERSION: &'static str = "1.2.0";
+const VERSION: &'static str = "1.3.0";
 
 const USAGE: &'static str = "
 Synchronize app files with Dropbox.
